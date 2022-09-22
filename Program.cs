c8d601a733ac207847f088e48bfdb49f0ed054f7
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-09-22 15:24:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Random rnd = new();
+/*
+ConsoleColor szin = (ConsoleColor)rnd.Next(1, 16);
+
+Console.Write("írd be a neved: ");
+string nev = Console.ReadLine();
+
+Console.Write("Szia ");
+Console.ForegroundColor = szin;
+Console.Write(nev);
+Console.ResetColor();
+Console.Write("!\nHogy vagy?");
+*/
+
+Console.SetWindowSize(90, 19);
+
+Console.Write("írd be a neved: ");
+string nev = Console.ReadLine();
+Console.Clear();
+
+Console.SetCursorPosition(
+    left: Console.WindowWidth / 2 - nev.Length / 2,
+    top: Console.WindowHeight / 2);
+
+for (int i = 0; i < nev.Length; i++)
+{
+    Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+    Thread.Sleep(200);
+    Console.Write(nev[i]);
+}
+
+Console.Write("\n");
+Console.ReadKey(true);
